0585cd12087a0f54606718a3b9be62e5425d19fb
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-28 14:37:32 +1300

    added server.js file for api routes & middlewares

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const express = require('express')
+const server = express()
+const cors = require('cors')
+
+// const home = require('./routes/home')
+
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, '../client/built')))
+
+
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/built/index.html'))
+})
+
+module.exports = server
