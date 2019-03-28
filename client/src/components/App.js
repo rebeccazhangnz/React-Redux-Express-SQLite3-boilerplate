df08b33258f1d4a62960b8294755ba983048ef4c
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-28 14:35:53 +1300

    Added main App component

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <div>
+        <h1>This is the App component</h1>
+    </div>
+  )
+}
+
+export default App
